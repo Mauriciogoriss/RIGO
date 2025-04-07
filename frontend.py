e70d0815cc20ac462b046c6e72c8d47239ef91ff
berniquintana05 <berny_quintana@yahoo.com.mx>
2025-04-07 10:32:11 -0600

    Agregar code a rama

---
+++ frontend.py
@@ -0,0 +1,14 @@
+print("Hola equipo, este es el entregable de hoy")
+
+a = 16
+b = 43
+c = 135
+
+def multi():
+    print(a*b*c)
+multi()
+
+def mostrar():
+    print("Esta rama esta destinada para los cambios necesarios del frontend")
+
+mostrar()
